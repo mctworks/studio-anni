de5061da0585585b5d2b08491232d926be6884e5
Michael Thompson <mcthompson1981@gmail.com>
2018-11-30 19:37:06 -0500

    Added "Meet Anni" section of index.php. New site BG color.

---
+++ head.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-PKPK5SB');</script>
+<!-- End Google Tag Manager -->
+      <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
+      <title>Studio Anni - The Art of Anni Thompson</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		  <meta name="description" content="Art of Atlanta-based artist Anni Thompson, who specializes in realistic acrylic paintings of pet portraits, space, nature and fantasy themes."/>
+		  <meta name="keywords" content="art, pet portraits, acrylic paintings, atlanta artists, photorealistic art, pet paintings, animal paintings, space paintings, dog portraits, cat portraits, anni thompson, studio anni"/>
+		  <meta name="p:domain_verify" content="d58df890b99f9ba7e1f98ee4cc2fcef0"/>
+      <link href='https://fonts.googleapis.com/css?family=Della+Respira' rel='stylesheet' type='text/css'>
+      <link href='https://fonts.googleapis.com/css?family=Oswald:300' rel='stylesheet' type='text/css'>
+      <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Waiting+for+the+Sunrise" rel="stylesheet">
+      <link href="view/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="all">
+      <link href="view/customsa.css" rel="stylesheet" media="all">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+      <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+      <script src="view/bootstrap/js/bootstrap.min.js"></script>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+      <link rel="stylesheet" href="view/bootstrap/css/bootstrap-formhelpers-min.css" media="all">
+      <link rel="stylesheet" href="view/bootstrap/css/bootstrapValidator-min.css"/>
+      <link rel="stylesheet" href="view/bootstrap/css/bootstrap-side-notes.css" />
+      <link rel="stylesheet" href="stripe.css" />
+      <script src="view/script.js"></script>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+      <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+      <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+		  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+      <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick-theme.css"/>
+	</head>
+<body>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PKPK5SB"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 //main
 require_once ('southwinds/phoenixeyes.php');
-include ('view/headfoot.php');
+include ('head.php');
+include ('nav.php');
+include ('footer.php');
 
 //get recent slides
 $query = "SELECT name, pieceID, size, slideimg FROM works
@@ -27,7 +29,7 @@ $statement->closeCursor();
       $slide_url = "slides/" . $piece['slideimg'] . ".jpg";
       $page_url = "piece.php?id=" . $piece['pieceID'];
       echo '<div><a href="' . $page_url . '" target="_blank"><img src="' . $slide_url . '" class="responsive-slide">'.
-      '<span class="slide-text"><h3>' . $piece['name'] . '</h3>' . $piece['size'] . '</span></div>';
+      '<span class="slide-text"><h3>' . $piece['name'] . '</h3>' . $piece['size'] . '</span></a></div>';
     }?>
   </div>
 </div>
@@ -46,5 +48,23 @@ $statement->closeCursor();
         });
       });
     </script>
+<h1>Meet Anni!</h1>
+<div class='index-body'>
+  <div class='anni-photo'><img src='aboutanni.jpg' class='img-responsive' alt='Anni Thompson'></div>
+  <div class='about-anni'><p>Anni Thompson is a professional artist in the Atlanta area who specializes
+    in original acrylic paintings and custom work. The themes of her work include animals, space, nature,
+    fantasy, and everyday people. Anni has meticulous eye for minor details, aiming for realism and the
+    use of vibrant colors to capture not only the look of the subject, but also its essence and feel.
+    While her influences include Maggie Stiefvater, her aunt and sometimes-mentor Bepe Kafka, as well as
+    Vincent Van Gogh, Anni prefers to focus on refining her own proven style rather than fixating on the
+    style of other artists.</p><p>Anni is not only an avid reader, but a self proclaimed nerd and animal lover. She lives with her
+    husband and four cats, and spends a lot of time caught up in her imagination. She aims to spread joy
+    through her work and hopes that any time you see her artwork, it makes you feel a little bit better.
+    She would love it if you followed her on social media and shared her artwork with others who might enjoy it.</p>
+    <p><a href="https://www.facebook.com/StudioAnniLLC" target="_blank" style="text-decoration: none;"><i class="fab fa-facebook fa-2x"></i><span style="font-family: 'Waiting for the Sunrise', sans-serif;font-size: 2em;"> StudioAnniLLC</span></a><br />
+       <a href="https://www.instagram.com/studio_anni/" target="_blank" style="text-decoration: none;"><i class="fab fa-instagram fa-2x"></i><span style="font-family: 'Waiting for the Sunrise', sans-serif;font-size: 2em;"> studio_anni</span></a><br />
+       <a href="https://twitter.com/StudioAnni" target="_blank" style="text-decoration: none;"><i class="fab fa-twitter fa-2x"></i><span style="font-family: 'Waiting for the Sunrise', sans-serif;font-size: 2em;"> StudioAnni</span></a></p>
+  </div>
+</div>
 </body>
 </html>
