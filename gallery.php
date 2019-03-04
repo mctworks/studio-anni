00a5c14f06c10d1a4ca476c19e597a7693ecf61e
Michael Thompson <mcthompson1981@gmail.com>
2019-03-04 14:24:50 -0500

    Ironed out some cosmetic issues with the gallery pages.

--- gallery.php
+++ gallery.php
@@ -17,14 +17,18 @@ $statement->closeCursor();
   <div class="gallery-header"><h1>Main Gallery</h1></div>
     <div class="gallery">
         <?php foreach($result as $piece){
-            echo '<div class="gallery-selection"><div class="piece-image"><a href ="piece.php?id=' . $piece['pieceID'] . '">' . '<img src="gallery/' . $piece['image'] . '.jpg" class="img-responsive" alt="Click To See More"></a></div>' .
-                '<div class="piece-details"><p><b>' . $piece['name'] . '</b></br>
-                    <b>Size/Medium: </b>' . $piece['size'] . '</br>
-                    <b>Price: $</b>' . $piece['price'] . '</br>';
+            echo '<div class="gallery-selection">
+                    <div class="piece-image">
+                      <a href ="piece.php?id=' . $piece['pieceID'] . '">' . '<img src="gallery/' . $piece['image'] . '.jpg" class="img-responsive" alt=' .  $piece['name'] . '></a>' .
+                    '</div>'; //</piece-image>
+                echo '<div class="piece-details">
+                      <p><h4>' . $piece['name'] . '</h4>
+                      <b>Size/Medium: </b>' . $piece['size'] . '</br>
+                      <b>Price: $</b>' . $piece['price'] . '</br>';
                     if ($piece['society6'] == 1){
                       echo '<em>Prints of this available at <a href="https://society6.com/studioanni" target="_blank">Anni\'s Society6</a>!</em></p>';
-                    } else { echo '</p>'; }
-                echo '</div></div>';
+                    } else { echo '</br></p>'; }
+                echo '</div></div>'; //</piece-details></gallery-selection>
             }
         ?>
     </div>
--- special.php
+++ special.php
@@ -16,13 +16,17 @@ $statement->closeCursor();
   <div class="gallery-header"><h1>Special Gallery</h1></div>
   <div class="gallery">
       <?php foreach($result as $piece){
-          echo '<div class="gallery-selection"><div class="piece-image"><a href ="piece.php?id=' . $piece['pieceID'] . '">' . '<img src="gallery/' . $piece['image'] . '.jpg" class="img-responsive" alt="Click To See More"></a></div>' .
-              '<div class="piece-details"><p><b>' . $piece['name'] . '</b></br>
-                  <b>Size/Medium: </b>' . $piece['size'] . '</br>' .
-                  '<b>Status: </b>' . $piece['specialstatus'] . '</br>';
+          echo '<div class="gallery-selection">
+                  <div class="piece-image">
+                    <a href ="piece.php?id=' . $piece['pieceID'] . '">' . '<img src="gallery/' . $piece['image'] . '.jpg" class="img-responsive" alt="' . $piece['name'] . '""></a>
+                  </div>' .
+                  '<div class="piece-details">
+                    <p><h4>' . $piece['name'] . '</h4></br>
+                    <b>Size/Medium: </b>' . $piece['size'] . '</br>' .
+                    '<b>Status: </b>' . $piece['specialstatus'] . '</br>';
                   if ($piece['society6'] == 1){
                     echo '<em>Prints of this available at <a href="https://society6.com/studioanni" target="_blank">Anni\'s Society6</a>!</em></p>';
-                  } else { echo '</p>'; }
+                  } else { echo '</br></p>'; }
                   echo '</div></div>';
               }
       ?>
