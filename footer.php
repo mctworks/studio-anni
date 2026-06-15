b479377e98a13a08fe7cea63bd730cca315f6cdf
Michael C. Thompson <mcthompson1981@gmail.com>
2026-06-15 19:34:06 -0400

    Added links to MCT630.com

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <?php //Includes body and html closers. Should work on most pages. ?>
 <footer class="navbar navbar-inverse navbar-fixed-bottom" role="navigation" id="footer">
-		<div class="navbar-text pull-left">
-			<p>&copy;<?php echo date("Y") ?> Studio Anni LLC.</p>
+		<div class="navbar-text pull-left footer-text">
+			<p>&copy;<?php echo date("Y") ?> Studio Anni LLC. &emsp; <a href="https://www.mct630.com/" target="_blank"><img src="mct630_logo.svg" width="40" alt="MCT630 Logo"></a> Development by <a href="https://www.mct630.com/" target="_blank">MCT630</a>.</p>
 		</div>
 		<div class="navbar-text pull-right">
 			<!-- <a href="https://www.facebook.com/StudioAnniLLC"><img src="fbicon.gif"></a>
